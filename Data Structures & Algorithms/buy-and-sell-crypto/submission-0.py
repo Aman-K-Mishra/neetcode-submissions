9c543b5fc8fc20eac72b6c3fe01bdf69e5cf648e
Aman Kumar Mishra <148127870+Aman-K-Mishra@users.noreply.github.com>
2026-08-01 01:21:15 +0530

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -3,5 +3,4 @@ class Solution:
         maxp = 0
         for s in range(0, len(prices)):
             maxp = max(maxp, prices[s] - min(prices[0:s+1]))
-            print(s,min(prices[0:s+1]))
         return maxp
